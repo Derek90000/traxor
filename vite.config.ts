8bdb3801f649db8f0df0f3624355d389b74e441a
Derek90000 <dereknel900@gmail.com>
2025-06-24 08:32:49 -0400

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,13 +7,4 @@ export default defineConfig({
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8888',
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
 });
